3657bc6cdb250cadd6f5d1fbaabcf70f32816f2b
dc-21 <cholahkuboko@gmail.com>
2024-11-03 14:32:46 +0200

    updated auth and return type

--- Backend/Services/auth.js
+++ Backend/Services/auth.js
@@ -5,7 +5,7 @@ const adminCollection = db.collection("users");
 
 const registerUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { fullname, email, password, phone } = req.body;
 
     // Check if admin already exists
     const querySnapshot = await adminCollection
@@ -19,7 +19,12 @@ const registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Save admin to Firestore using an auto-generated ID
-    await adminCollection.add({ email, password: hashedPassword });
+    await adminCollection.add({
+      fullname,
+      email,
+      password: hashedPassword,
+      phone,
+    });
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -43,13 +48,21 @@ const loginUser = async (req, res) => {
 
     const doc = querySnapshot.docs[0];
 
+    const user = {
+      fullname: doc.data().fullname,
+      email: doc.data().email,
+      phone: doc.data().phone,
+    };
+
     // Validate password
     const isValid = await bcrypt.compare(password, doc.data().password);
     if (!isValid) {
       return res.status(400).json({ message: "Invalid password" });
     }
 
-    res.status(200).json({ message: "User logged in successfully" });
+    res
+      .status(200)
+      .json({ message: "User logged in successfully", user: user });
   } catch (error) {
     res.status(500).json({ message: "Error logging in", error: error.message });
   }
